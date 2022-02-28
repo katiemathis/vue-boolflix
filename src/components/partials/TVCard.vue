d8bb9c57a262ff8d1833d2c6287c56d2c3589777
katiepalumbo <palumbo.katie@gmail.com>
2022-02-28 23:19:03 +0100

    milestone 4 completato

--- src/components/partials/TVCard.vue
+++ src/components/partials/TVCard.vue
@@ -1,31 +1,55 @@
 <template>
 <!-- v-for gets completed on app.vue once all $emits and props are properly identified throughout files-->
 <!-- using {{}} here takes each given object from the array and prints it on the screen -->
-        <div class="TVSeries col" >
-            <div class="row row-cols-lg-4 row-cols-md-2 row-cols-1">
-                <div>
+      <!--  <div class="TVSeries col" >
+            <div class="row row-cols-lg-10 row-cols-md-5 row-cols-2">
+                <div class="tv-poster">
                     <div v-if="TVSeries.poster_path==null">{{TVSeries.name}}</div>
                     <div v-else><img :src= "'http://image.tmdb.org/t/p/w154/' + TVSeries.poster_path" :alt="TVSeries.name"></div>
                 </div>
-                <!--<div>{{TVSeries.name}}</div>
-                <div>{{TVSeries.original_name}}</div>
-                <div v-if= "TVSeries.original_language==''">{{TVSeries.original_language}}</div>
-                <div v-else><lang-flag :iso= "TVSeries.original_language" /></div>
-                <div>
-                    <i class="fa-solid fa-star" v-for="(rating, index) in fiveStarRating" :key="index"></i>
-                    <i class="fa-solid fa-star-half" v-for="(rating, index) in fiveStarRating%1" :key="index"></i>
-                </div>-->
+                <div class="tv-details hide">
+                    <div>{{TVSeries.name}}</div>
+                    <div>{{TVSeries.original_name}}</div>
+                    <div v-if= "TVSeries.original_language==''">{{TVSeries.original_language}}</div>
+                    <div v-else><lang-flag :iso= "TVSeries.original_language" /></div>
+                    <div class="tv-rating">
+                        <i class="fa-solid fa-star" v-for="(rating, index) in fiveStarRating" :key="index"></i>
+                        <i class="fa-solid fa-star-half" v-for="(rating, index) in fiveStarRating%1" :key="index"></i>
+                    </div>
+                </div>
+            </div>
+        </div> -->
+        <div class="tv-series col">
+            <div class="flip-card row justify-content-start">
+                <div class="col flip-card-inner">
+                    <div class="flip-card-front">
+                        <div v-if="TVSeries.poster_path==null">{{TVSeries.name}}</div>
+                        <div v-else><img :src= "'http://image.tmdb.org/t/p/w185/' + TVSeries.poster_path" :alt="TVSeries.name"></div>
+                    </div>
+                    <div class="movie-info flip-card-back">
+                        <div><h6>{{TVSeries.name}}</h6></div>
+                        <div class="original-title" :class= "(TVSeries.original_name==TVSeries.name)?'hide':''"><span class="bold">Original Name: </span>{{TVSeries.original_name}}</div>
+                        <div v-if= "TVSeries.original_language==''">{{TVSeries.original_language}}</div>
+                        <div v-else><lang-flag :iso= "TVSeries.original_language" /></div>
+                        <div class="star-ratings">
+                            <span :class= "(fiveStarRating==0)?'hide':''"><span class="bold">Avg Rating: </span><i class="fa-solid fa-star" v-for="(rating, index) in fiveStarRating" :key="index"></i></span>
+                            <span :class= "(!fiveStarRating==0)?'hide':''"><span class="bold">Be the first to rate: </span><i class="fa-regular fa-star"></i></span>
+                        </div>
+                        <div class="overview"><span class="bold" :class= "(TVSeries.overview=='')?'hide':''">Overview: </span>{{TVSeries.overview}}</div>
+                    </div>
+                </div>
             </div>
         </div>
 </template>
 
 <script>
-//import LangFlag from 'vue-lang-code-flags';
+import LangFlag from 'vue-lang-code-flags';
+
 export default {
     name: 'TVCard',
-    /*components: {
+    components: {
         LangFlag,
-    },*/
+    },
     props: {
         'TVList': Array,
         'TVSeries': Object,
@@ -45,16 +69,93 @@ export default {
 
 @import '../../style/general.scss';
 
+
 *{
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 }
 
-.TVSeries {
-    width: 100%;
-    list-style-type: none;
-    background-color: $colorBackground;
+.show {
+    display: block;
+}
+
+.hide {
+    display: none;
+}
+
+.bold {
+    font-weight: 600;
+}
+
+.flip-card {
+    background-color: transparent;
+    padding: 0;
+    width: 185px;
+    height: 278px;
+    perspective: 1000px;
+    &:hover .flip-card-inner {
+        transform: rotateY(-180deg);
+        cursor: pointer;
+    }
+
+    .flip-card-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.8s;
+        transform-style: preserve-3d;
+        
+
+        .flip-card-front {
+            position: absolute;
+            padding: 0;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+            -webkit-backface-visibility: hidden; /* Safari */
+            backface-visibility: hidden;
+            background-color: $colorHeader;
+            color: #fff;
+        }
+        .flip-card-back {
+            position: absolute;
+            padding: 10px;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+            -webkit-backface-visibility: hidden; /* Safari */
+            backface-visibility: hidden;
+            background-color: $colorHeader;
+            color: #fff;
+            transform: rotateY(180deg);
+            text-align: left;
+            
+            .star-ratings {
+                color: #fff;
+                font-size: small;
+                .fa-star {
+                    color: gold;
+                }
+                .fa-regular {
+                    color: #fff;
+                }
+
+            }
+            .original-title {
+                font-style: italic;
+                font-size: small;
+            }
+            .overview {
+                font-size: small;
+                overflow-y: scroll;
+            }
+        }
+    }
+
+    
+
 }
 
 
